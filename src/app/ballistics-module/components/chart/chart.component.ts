1ca0f789defffb1a605935ce9cf9948f7c446a76
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-10-01 13:08:13 -0400

    Change PDF to display in browser

--- src/app/ballistics-module/components/chart/chart.component.ts
+++ src/app/ballistics-module/components/chart/chart.component.ts
@@ -14,61 +14,67 @@ import atmospherics from 'pg-atmospherics';
 import { DataService } from '../../services/data.service';
 
 @Component({
-	selector: 'app-chart',
-	styleUrls: ['./chart.component.css'],
-	templateUrl: './chart.component.html'
+    selector: 'app-chart',
+    styleUrls: ['./chart.component.css'],
+    templateUrl: './chart.component.html'
 })
 export class ChartComponent implements OnInit {
 
-	firearms: Firearm[] = null;
-	firearm: Firearm = null;
-	isOpen = true;
+    firearms: Firearm[] = null;
+    firearm: Firearm = null;
+    isOpen = true;
     ranges: Range[] = null;
-	round: Round = null;
-	showMil = true;
-	showMoA = true;
-	showIPHY = true;
+    round: Round = null;
+    showMil = true;
+    showMoA = true;
+    showIPHY = true;
     speedOfSound = 0;
     tableHtml = null;
-	target: Target = null;
-	weather: Weather = null;
+    target: Target = null;
+    weather: Weather = null;
 
-	constructor(public dataService: DataService) {}
+    constructor(public dataService: DataService) { }
 
     print() {
         const pdf = new jsPDF('p', 'mm', 'a4');
-        pdf.text([`Range Chart - Firearm (${this.firearm.name}) - Round (${this.round.name})`, ``], 104, 10, { align: 'center'});
+        pdf.text([`Range Chart - Firearm (${this.firearm.name}) - Round (${this.round.name})`, ``], 104, 10, { align: 'center' });
         // @ts-ignore 2339
-        pdf.autoTable({ html: '#ballisticsTable', margin: 1, startY: 20, styles: { fontSize: 9, cellPadding: 1 }});
-        pdf.save(`Range Chart - Firearm (${this.firearm.name}) - Round (${this.round.name}).pdf`);
+        pdf.autoTable({ html: '#ballisticsTable', margin: 1, startY: 20, styles: { fontSize: 9, cellPadding: 1 } });
+        const pdfString = pdf.output('datauristring');
+        const embed = `<embed width='100%' height='100%' src='${pdfString}'/>`;
+        const x = window.open();
+        x.document.open();
+        x.document.write(embed);
+        x.document.close();
+        // pdf.save(`Range Chart - Firearm (${this.firearm.name}) - Round (${this.round.name}).pdf`);
     }
 
-	ngOnInit() {
-		this.dataService.getWeather().subscribe(weather => {
+    ngOnInit() {
+        this.dataService.getWeather().subscribe(weather => {
             this.weather = weather;
             this.speedOfSound = atmospherics.speedOfSound(this.weather.altitudeFeet);
         });
-		this.dataService.getTarget().subscribe(target => {
+        this.dataService.getTarget().subscribe(target => {
             this.target = target;
         });
-		this.dataService.getFirearms().subscribe(firearms => {
+        this.dataService.getFirearms().subscribe(firearms => {
             this.firearms = firearms;
         });
-		this.dataService.getFirearmId().subscribe(firearmId => {
-            if(this.firearms != null && firearmId != null) {
-                this.firearm = this.firearms.find(f => f.id===firearmId);
-                this.showMil = this.firearm.turretUnits==='Mil' || this.firearm.reticleUnits==='Mil';
-                this.showMoA = this.firearm.turretUnits==='MoA' || this.firearm.reticleUnits==='MoA';
-                this.showIPHY = this.firearm.turretUnits==='IPHY' || this.firearm.reticleUnits==='IPHY';
+        this.dataService.getFirearmId().subscribe(firearmId => {
+            if (this.firearms != null && firearmId != null) {
+                this.firearm = this.firearms.find(f => f.id === firearmId);
+                this.showMil = this.firearm.turretUnits === 'Mil' || this.firearm.reticleUnits === 'Mil';
+                this.showMoA = this.firearm.turretUnits === 'MoA' || this.firearm.reticleUnits === 'MoA';
+                this.showIPHY = this.firearm.turretUnits === 'IPHY' || this.firearm.reticleUnits === 'IPHY';
             }
         });
-		this.dataService.getRoundId().subscribe(roundId => {
-            if(this.firearm != null && roundId != null) {
-                this.round = this.firearm.rounds.find(r => r.id===roundId);
+        this.dataService.getRoundId().subscribe(roundId => {
+            if (this.firearm != null && roundId != null) {
+                this.round = this.firearm.rounds.find(r => r.id === roundId);
             }
         });
-		this.dataService.getRanges().subscribe(ranges => {
+        this.dataService.getRanges().subscribe(ranges => {
             this.ranges = ranges;
         });
-	}
+    }
 }
