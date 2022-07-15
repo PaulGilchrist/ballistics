bd01c8be608bf47b31a60dc60e64d1d314d4eb03
paulgilchrist <PGilchrist@pulte.com>
2022-07-15 12:25:04 -0400

    Update initial data

--- src/data/firearms.js
+++ src/data/firearms.js
@@ -1,5 +1,206 @@
 const FIREARMS = [
-	{ "id": "8f331a2c-ef70-4a64-a028-276bf27523c4", "name": "10/22", "elevationTurretGradients": 10, "reticleUnits": "Mil", "rounds": [{ "id": "e67e903e-2a22-4cfc-a1a9-e9c92adf6e5b", "name": "Federal Champion ", "bulletBC": 0.114, "bulletDiameterInches": 0.22, "bulletWeightGrains": 36, "muzzleVelocityFPS": 1260 }], "sightHeightInches": 1.5, "turretUnits": "Mil", "windageTurretGradients": 10, "zeroRangeYards": 50, "zeroRange": 100, "zeroRangeUnits": "Yards" }, { "id": "8a6b05a9-ecb7-4f62-8a73-c07df86209e5", "name": "CTR/Reese", "elevationTurretGradients": 2, "reticleUnits": "MoA", "rounds": [{ "id": "ba665537-7653-43c0-82a6-6410972fee09", "name": "FGMM 69", "bulletBC": 0.301, "bulletDiameterInches": 0.224, "bulletWeightGrains": 69, "muzzleVelocityFPS": 2810 }, { "id": "a931c049-7771-45f6-8ede-5ea9d7674a2d", "name": "Handload 77", "bulletBC": 0.372, "bulletDiameterInches": 0.224, "bulletWeightGrains": 77, "muzzleVelocityFPS": 2680 }, { "id": "fe7c8d3e-3ed2-4614-832d-9dd4833fe4db", "name": "TAP 5.56", "bulletBC": 0.354, "bulletDiameterInches": 0.224, "bulletWeightGrains": 75, "muzzleVelocityFPS": 2680 }], "sightHeightInches": 2, "turretUnits": "MoA", "windageTurretGradients": 2, "zeroRangeYards": 100, "zeroRange": 100, "zeroRangeUnits": "Yards" }, { "id": "18f74ec3-10d7-44c9-8add-369e06507cdc", "name": "ELR", "sightHeightInches": 2, "zeroRange": 100, "zeroRangeUnits": "Yards", "reticleUnits": "Mil", "elevationTurretGradients": 10, "turretUnits": "Mil", "windageTurretGradients": 10, "rounds": [{ "id": "f1028fdd-c82f-4f78-bfd7-bee52d796b9a", "name": "6.5 Creedmoor", "bulletDiameterInches": ".264", "bulletWeightGrains": "130", "muzzleVelocityFPS": "2610", "bulletBC": ".584" }] }, { "id": "0d5782a2-dd5c-4395-a98b-4842f53eebbf", "name": "LTR", "elevationTurretGradients": 10, "reticleUnits": "Mil", "rounds": [{ "id": "4c2574f0-d0e0-4889-a7ed-abc293da06df", "name": "FGMM 168", "bulletBC": 0.462, "bulletDiameterInches": 0.308, "bulletWeightGrains": 168, "muzzleVelocityFPS": 2524 }], "sightHeightInches": 1.5, "turretUnits": "Mil", "windageTurretGradients": 10, "zeroRangeUnits": "Yards", "zeroRange": 100 }, { "id": "9f1598a3-ec38-4bc2-8f81-333b81d30967", "name": "Marlin 336", "elevationTurretGradients": 4, "reticleUnits": "MoA", "rounds": [{ "id": "40fd0f30-7f60-4bf0-b7e8-7c5fd05533da", "name": "Winchester 150", "bulletBC": 0.218, "bulletDiameterInches": 0.308, "bulletWeightGrains": 150, "muzzleVelocityFPS": 2290 }, { "id": "16cd6950-f41d-439f-98c2-340d201a0e3a", "name": "Winchester 170", "bulletBC": 0.241, "bulletDiameterInches": 0.308, "bulletWeightGrains": 170, "muzzleVelocityFPS": 2200 }], "sightHeightInches": 1, "turretUnits": "MoA", "windageTurretGradients": 4, "zeroRangeUnits": "Yards", "zeroRange": 100 }, { "id": "7d7f9e4d-7c33-4e04-ad1e-602e6f5d35f9", "name": "OBR", "elevationTurretGradients": 10, "reticleUnits": "Mil", "rounds": [{ "id": "fb97d392-97cc-4289-80ad-fc1fc50e4ab3", "name": "FGGM 168", "bulletBC": 0.462, "bulletDiameterInches": 0.308, "bulletWeightGrains": 168, "muzzleVelocityFPS": 2650 }, { "id": "0dadbe8f-599e-4fce-8a1f-0e273e5ab535", "name": "FGMM 175", "bulletBC": 0.505, "bulletDiameterInches": 0.308, "bulletWeightGrains": 175, "muzzleVelocityFPS": 2600 }], "sightHeightInches": 2, "turretUnits": "Mil", "windageTurretGradients": 10, "zeroRangeYards": 100, "zeroRange": 100, "zeroRangeUnits": "Yards" }, { "id": "479a565e-cfe9-4ddf-8bf2-292cf1b0ca4f", "name": "SPR", "elevationTurretGradients": 10, "reticleUnits": "Mil", "rounds": [{ "id": "93a7b0dc-c106-4fd5-8c68-16aa0caa7165", "name": "FGMM 69", "bulletBC": 0.301, "bulletDiameterInches": 0.224, "bulletWeightGrains": 69, "muzzleVelocityFPS": 2950 }, { "id": "abe8f65c-9e6b-4adf-8625-4f4ceab8cb1f", "name": "FGMM 77", "bulletBC": 0.372, "bulletDiameterInches": 0.224, "bulletWeightGrains": 77, "muzzleVelocityFPS": 2720 }, { "id": "1f3303a9-8bd9-4e0b-bc4b-002073c2edf8", "name": "Handload 77", "bulletBC": 0.372, "bulletDiameterInches": 0.224, "bulletWeightGrains": 77, "muzzleVelocityFPS": 2760 }, { "id": "98949310-8370-4807-af7a-16711831d7d7", "name": "TAP 5.56", "bulletBC": 0.354, "bulletDiameterInches": 0.224, "bulletWeightGrains": 75, "muzzleVelocityFPS": 2821 }], "sightHeightInches": 1.5, "turretUnits": "Mil", "windageTurretGradients": 10, "zeroRangeYards": 100, "zeroRange": 100, "zeroRangeUnits": "Yards" }
+	{
+		"id": "8f331a2c-ef70-4a64-a028-276bf27523c4",
+		"name": "10/22",
+		"elevationTurretGradients": 10,
+		"reticleUnits": "Mil",
+		"rounds": [
+			{
+				"id": "e67e903e-2a22-4cfc-a1a9-e9c92adf6e5b",
+				"name": "Federal Champion ",
+				"bulletBC": 0.114,
+				"bulletDiameterInches": 0.22,
+				"bulletWeightGrains": 36,
+				"muzzleVelocityFPS": 1260
+			}
+		],
+		"sightHeightInches": 1.5,
+		"turretUnits": "Mil",
+		"windageTurretGradients": 10,
+		"zeroRangeYards": 50,
+		"zeroRange": 100,
+		"zeroRangeUnits": "Yards"
+	},
+	{
+		"id": "8a6b05a9-ecb7-4f62-8a73-c07df86209e5",
+		"name": "CTR/Reese",
+		"elevationTurretGradients": 2,
+		"reticleUnits": "MoA",
+		"rounds": [
+			{
+				"id": "ba665537-7653-43c0-82a6-6410972fee09",
+				"name": "FGMM 69",
+				"bulletBC": 0.301,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 69,
+				"muzzleVelocityFPS": 2810
+			},
+			{
+				"id": "a931c049-7771-45f6-8ede-5ea9d7674a2d",
+				"name": "Handload 77",
+				"bulletBC": 0.372,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 77,
+				"muzzleVelocityFPS": 2680
+			},
+			{
+				"id": "fe7c8d3e-3ed2-4614-832d-9dd4833fe4db",
+				"name": "TAP 5.56",
+				"bulletBC": 0.354,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 75,
+				"muzzleVelocityFPS": 2680
+			}
+		],
+		"sightHeightInches": 2,
+		"turretUnits": "MoA",
+		"windageTurretGradients": 2,
+		"zeroRangeYards": 100,
+		"zeroRange": 100,
+		"zeroRangeUnits": "Yards"
+	},
+	{
+		"id": "18f74ec3-10d7-44c9-8add-369e06507cdc",
+		"name": "Other",
+		"sightHeightInches": 2,
+		"zeroRange": 100,
+		"zeroRangeUnits": "Yards",
+		"reticleUnits": "Mil",
+		"elevationTurretGradients": 10,
+		"turretUnits": "Mil",
+		"windageTurretGradients": 10,
+		"rounds": [
+			{
+				"id": "f1028fdd-c82f-4f78-bfd7-bee52d796b9a",
+				"name": "6.5 Creedmoor",
+				"bulletDiameterInches": 0.264,
+				"bulletWeightGrains": 130,
+				"muzzleVelocityFPS": 2610,
+				"bulletBC": 0.584
+			},
+			{
+				"id": "06e005cd-7f58-4034-b777-6a8efe9690a0",
+				"name": "300 Blk Subsonic",
+				"bulletDiameterInches": 0.308,
+				"bulletWeightGrains": 220,
+				"muzzleVelocityFPS": 1050,
+				"bulletBC": 0.654
+			},
+			{
+				"id": "6a5cd5b6-c687-4bbe-92e8-99ec38a78b4d",
+				"name": "300 Blk Supersonic",
+				"bulletDiameterInches": 0.308,
+				"bulletWeightGrains": 110,
+				"muzzleVelocityFPS": 2200,
+				"bulletBC": 0.290
+			},
+			{
+				"id": "40fd0f30-7f60-4bf0-b7e8-7c5fd05533da",
+				"name": "30-30",
+				"bulletBC": 0.218,
+				"bulletDiameterInches": 0.308,
+				"bulletWeightGrains": 150,
+				"muzzleVelocityFPS": 2290
+			}
+		]
+	},
+	{
+		"id": "0d5782a2-dd5c-4395-a98b-4842f53eebbf",
+		"name": "LTR",
+		"elevationTurretGradients": 10,
+		"reticleUnits": "Mil",
+		"rounds": [
+			{
+				"id": "4c2574f0-d0e0-4889-a7ed-abc293da06df",
+				"name": "FGMM 168",
+				"bulletBC": 0.462,
+				"bulletDiameterInches": 0.308,
+				"bulletWeightGrains": 168,
+				"muzzleVelocityFPS": 2524
+			}
+		],
+		"sightHeightInches": 1.5,
+		"turretUnits": "Mil",
+		"windageTurretGradients": 10,
+		"zeroRangeUnits": "Yards",
+		"zeroRange": 100
+	},
+	{
+		"id": "7d7f9e4d-7c33-4e04-ad1e-602e6f5d35f9",
+		"name": "OBR",
+		"elevationTurretGradients": 10,
+		"reticleUnits": "Mil",
+		"rounds": [
+			{
+				"id": "fb97d392-97cc-4289-80ad-fc1fc50e4ab3",
+				"name": "FGGM 168",
+				"bulletBC": 0.462,
+				"bulletDiameterInches": 0.308,
+				"bulletWeightGrains": 168,
+				"muzzleVelocityFPS": 2650
+			},
+			{
+				"id": "0dadbe8f-599e-4fce-8a1f-0e273e5ab535",
+				"name": "FGMM 175",
+				"bulletBC": 0.505,
+				"bulletDiameterInches": 0.308,
+				"bulletWeightGrains": 175,
+				"muzzleVelocityFPS": 2600
+			}
+		],
+		"sightHeightInches": 2,
+		"turretUnits": "Mil",
+		"windageTurretGradients": 10,
+		"zeroRangeYards": 100,
+		"zeroRange": 100,
+		"zeroRangeUnits": "Yards"
+	},
+	{
+		"id": "479a565e-cfe9-4ddf-8bf2-292cf1b0ca4f",
+		"name": "SPR",
+		"elevationTurretGradients": 10,
+		"reticleUnits": "Mil",
+		"rounds": [
+			{
+				"id": "93a7b0dc-c106-4fd5-8c68-16aa0caa7165",
+				"name": "FGMM 69",
+				"bulletBC": 0.301,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 69,
+				"muzzleVelocityFPS": 2950
+			},
+			{
+				"id": "abe8f65c-9e6b-4adf-8625-4f4ceab8cb1f",
+				"name": "FGMM 77",
+				"bulletBC": 0.372,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 77,
+				"muzzleVelocityFPS": 2720
+			},
+			{
+				"id": "1f3303a9-8bd9-4e0b-bc4b-002073c2edf8",
+				"name": "Handload 77",
+				"bulletBC": 0.372,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 77,
+				"muzzleVelocityFPS": 2760
+			},
+			{
+				"id": "98949310-8370-4807-af7a-16711831d7d7",
+				"name": "TAP 5.56",
+				"bulletBC": 0.354,
+				"bulletDiameterInches": 0.224,
+				"bulletWeightGrains": 75,
+				"muzzleVelocityFPS": 2821
+			}
+		],
+		"sightHeightInches": 1.5,
+		"turretUnits": "Mil",
+		"windageTurretGradients": 10,
+		"zeroRangeYards": 100,
+		"zeroRange": 100,
+		"zeroRangeUnits": "Yards"
+	}
 ]
 
 export default FIREARMS;
