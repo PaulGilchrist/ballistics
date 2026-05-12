15cc92369185ad3579a59211a94e685ae8f0e349
paulgilchrist <PGilchrist@pulte.com>
2026-05-12 14:30:26 -0400

    chore: remove unused useState imports from 6 component files

--- src/components/Firearms.jsx
+++ src/components/Firearms.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './form.css'
 
 const Firearms = ({firearms, onAdd, onSelect}) => {
--- src/components/Rounds.jsx
+++ src/components/Rounds.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './form.css'
 
 const Rounds = ({rounds, onAdd, onSelect}) => {
